2fe3a5c6edf0d2e7e21940cb1501cda2d255fe20
sv_shulga@ukr.net <sv_shulga@ukr.net>
2014-08-04 14:25:53 +0300

    last

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ session_start();
 define("ABS", $_SERVER["DOCUMENT_ROOT"]);
 require_once(ABS."/configs/config.php");
 
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-ini_set('error_reporting',  E_ALL);
-echo("OK");
 $app = new App();
 $app->debug = true;
 $app->run();

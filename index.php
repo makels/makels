71de6f1bfb5b10988e65afabf83dab68f94f3d39
Zerg <sv_shulga@ukr.net>
2014-08-04 12:48:59 +0300

    DB modify

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 session_start();
 require_once("/configs/config.php");
 
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('error_reporting',  E_ALL);
+
 $app = new App();
 $app->debug = true;
 $app->run();

289dcbcc822a028ce03c89938d0c9450a33c85d8
sv_shulga@ukr.net <sv_shulga@ukr.net>
2014-08-04 13:54:40 +0300

    last

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 ini_set('error_reporting',  E_ALL);
 
-$app = new App();
-$app->debug = true;
-$app->run();
+// $app = new App();
+// $app->debug = true;
+// $app->run();
 
 ?>

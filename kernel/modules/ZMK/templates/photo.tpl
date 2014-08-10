496c739cc0c5ccd121e459158ca27e2f3f65e158
sv_shulga@ukr.net <sv_shulga@ukr.net>
2014-08-10 21:53:24 +0300

    ZZZ

--- kernel/modules/ZMK/templates/photo.tpl
+++ kernel/modules/ZMK/templates/photo.tpl
@@ -1,9 +1,16 @@
 <!-- Photo screen -->
 <div id="zmk_mode_photo" class="zmk_modes">
 <div class="zmode_title">
-		<span>{"mod_zmk_mnu_photo"|label}</span>
+		<!--span>{*"mod_zmk_mnu_photo"|label*}</span-->
 	</div>
 	<div class="zmode_content">
-		<!-- Mode content -->
+		<div class="left-side">
+			{*module name="ZSlider"*}	
+		</div>
+		<div class="right-side">
+			<div>{module name="Lang"}</div>
+			<div>{module name="PhotoUploader"}</div>
+			<div>{module name="ChoiceModel"}</div>
+		</div>
 	</div>
 </div>
--- temp/eb92bf29cb6472049bbd2c0e8650e7465569c51a.file.photo.tpl.php
+++ temp/eb92bf29cb6472049bbd2c0e8650e7465569c51a.file.photo.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.16, created on 2014-08-06 17:25:31
+<?php /* Smarty version Smarty-3.1.16, created on 2014-08-10 21:51:29
          compiled from "D:\ORDERS\MAKELS\makels.com\kernel\modules\ZMK\templates\photo.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:1716753e23adb188fe8-54506595%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'eb92bf29cb6472049bbd2c0e8650e7465569c51a' => 
     array (
       0 => 'D:\\ORDERS\\MAKELS\\makels.com\\kernel\\modules\\ZMK\\templates\\photo.tpl',
-      1 => 1407088121,
+      1 => 1407696686,
       2 => 'file',
     ),
   ),
@@ -15,18 +15,27 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.16',
   'unifunc' => 'content_53e23adb1964f5_41749157',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_53e23adb1964f5_41749157')) {function content_53e23adb1964f5_41749157($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_label')) include 'D:\\ORDERS\\MAKELS\\makels.com\\kernel\\smarty\\plugins\\modifier.label.php';
+<?php if ($_valid && !is_callable('content_53e23adb1964f5_41749157')) {function content_53e23adb1964f5_41749157($_smarty_tpl) {?><?php if (!is_callable('smarty_function_module')) include 'D:\\ORDERS\\MAKELS\\makels.com\\kernel\\smarty\\plugins\\function.module.php';
 ?><!-- Photo screen -->
 <div id="zmk_mode_photo" class="zmk_modes">
 <div class="zmode_title">
-		<span><?php echo smarty_modifier_label("mod_zmk_mnu_photo");?>
-</span>
+		<!--span></span-->
 	</div>
 	<div class="zmode_content">
-		<!-- Mode content -->
+		<div class="left-side">
+				
+		</div>
+		<div class="right-side">
+			<div><?php echo smarty_function_module(array('name'=>"Lang"),$_smarty_tpl);?>
+</div>
+			<div><?php echo smarty_function_module(array('name'=>"PhotoUploader"),$_smarty_tpl);?>
+</div>
+			<div><?php echo smarty_function_module(array('name'=>"ChoiceModel"),$_smarty_tpl);?>
+</div>
+		</div>
 	</div>
 </div><?php }} ?>

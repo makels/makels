c66f4d47cf229bd8c87668a63ca9574786c29d93
sv_shulga@ukr.net <sv_shulga@ukr.net>
2014-08-24 20:21:38 +0300

    ZZZ

--- facedetect.php
+++ facedetect.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('error_reporting',  E_ALL);
+
 define("IMG_PATH", $_SERVER["DOCUMENT_ROOT"]."/themes/default/images/models/");
 
 if(!isset($_GET['model'] || !file_exists(IMG_PATH.$_GET['model'])) die("Model not found");

9023494c1b8e0ff18864a7f38e2987d1f400a437
sv_shulga@ukr.net <sv_shulga@ukr.net>
2014-08-27 18:02:52 +0300

    ZZZ

--- facedetect.php
+++ facedetect.php
@@ -7,7 +7,7 @@ ini_set('error_reporting',  E_ALL);
 define("IMG_PATH", $_SERVER["DOCUMENT_ROOT"]."/themes/default/images/models/");
 define("CASCADE_PATH", $_SERVER["DOCUMENT_ROOT"]."/api/opencv/cascades/haarcascades/");
 
-if(!isset($_GET['model'] || !file_exists(IMG_PATH.$_GET['model'])) die("Model not found");
+if(!isset($_GET['model'] || !file_exists(IMG_PATH.$_GET['model'])) { echo("Model not found"); die; }
 
 var_dump(face_count(IMG_PATH.$_GET['model'], CASCADE_PATH.'haarcascade_frontalface_alt.xml'));
 var_dump(face_detect(IMG_PATH.$_GET['model'], CASCADE_PATH.'haarcascade_frontalface_alt.xml'));
